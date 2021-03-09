47f2311a4875dc249e328de4e0a07713ada47129
Asia Mayfield <a.mayfield18@gmail.com>
2021-03-08 19:37:20 -0800

    working

--- src/App.js
+++ src/App.js
@@ -1,30 +1,57 @@
-import React, {usestate, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from "axios";
-import ReactDOM from 'react-dom'; 
-import Character from "./components/Character";
-const App = () => {
-  const [characters, setCharacters] = usestate ([]);
-  useEffect(() => {
-   const fetchData = () => {
-    axios
-    .get("https://swapi.dev/api/people" )
-    .then(res => setCharacters (res.data.message))
-    
-    .catch
-    
-    });
- 
-  ReactDOM.render(<Character />, document.getElementById("characters"));
 
-    
-  return (
-    <div className="App">
-      <h1 className="Header">XXCharacters</h1>
-    </div>
-    
-  );
+function App() {
+const [userData,setUserData]=useState([])
+
+useEffect(()=>{
+axios.get('https://swapi.dev/api/people/')
+.then(resultJSON=>{
+var numberOfResults = Object.keys(resultJSON).length;
+//console.log(numberOfResults);
+//console.log(JSON.stringify(resultJSON.data[1]));
+
+var character = resultJSON.data[5];
+//console.log(character['hair_color']);
+//console.log(JSON.stringify(resultJSON.data[5]['hair_color']));
+
+let i = 0;
+var characterArray = [];
+while (i < numberOfResults) {
+let name = JSON.stringify(resultJSON.data[i]['name']);
+let hairColor = JSON.stringify(resultJSON.data[i]['hair_color']);
+// console.log(name);
+//console.log(hairColor);
+let characterData = {
+name:name,
+hairColor:hairColor
+}
+characterArray.push(characterData);
+i++;
+}
+setUserData(characterArray);
+})
+
+.catch(err=>{
+  console.log(err);
+  })
+  },[])
+  const users=userData.map((data,id)=>{
+  return <div key={id}>
+  <h2>{data.name} {data.hairColor}</h2>
   
+  </div>
+  })
+  return (
+  <>
+  {users}
+  
+  </>
+  );
   }
-  export default App
- 
+  
+  
+  
+  
+  export default App; 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,9 +1,17 @@
 // Write your Character component here
-import React from "react"
+import React, {usestate, useEffect} from 'react';
+import axios from "axios";
 
-function Character(props) {
-    return (<h1>Hello,</h1>)
-    }
-    
-    
-export default Character    
+
+class Character extends React.Component {
+render() {
+return (
+<div>
+<h1>Character Name: {this.props.characterData.name}</h1>
+<h1>Character Hair Color: {this.props.characterData.hairColor}</h1>
+</div>
+)
+}
+}
+
+export default Character 
